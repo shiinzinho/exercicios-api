b8a12a1f4c991b5290ad43e86d90accff2df6518
shiinzinho <gabrieldosantoslacerda@gmail.com>
2023-08-03 12:33:19 -0300

    ex 5 upd

--- app/Http/Controllers/Exercicio5Controller.php
+++ app/Http/Controllers/Exercicio5Controller.php
@@ -2,23 +2,21 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\Exercicio5FormRequest;
 use Illuminate\Http\Request;
 
 class Exercicio5Controller extends Controller
 {
-    public function idade(Request $request)
+    public function divisivel(Exercicio5FormRequest $request)
     {
-        if ($request->numero <= 12) {
+
+        if ($request->n1 % 4 == 0) {
             return json_encode([
-                'menssage' => '“O usuário é uma criança'
-            ]);
-        } else if ($request->numero <= 18) {
-            return json_encode([
-                'menssage' => "É um adolescente"
+                'mensagem' => 'é divisivel por 4'
             ]);
         } else {
             return json_encode([
-                'menssage' => '“É um adulto'
+                'mensagem' => "não é divisivel por 4"
             ]);
         }
     }
