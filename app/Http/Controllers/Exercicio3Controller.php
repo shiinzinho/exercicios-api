4e2ad44d16c16c9c195cb8b68c8c6a9dc106404c
shiinzinho <gabrieldosantoslacerda@gmail.com>
2023-08-03 09:36:23 -0300

    ex 3 upd

--- app/Http/Controllers/Exercicio3Controller.php
+++ app/Http/Controllers/Exercicio3Controller.php
@@ -6,7 +6,6 @@ use Illuminate\Http\Request;
 
 class Exercicio3Controller extends Controller
 {
-<<<<<<< HEAD
     public function media(Request $request)
     {
 
@@ -20,7 +19,4 @@ class Exercicio3Controller extends Controller
             'resultado' => (($primeiroNumero + $segundoNumero + $terceiroNumero) / 3)
         ]);
     }
-=======
-    //
->>>>>>> f3e9615e8de9b7af328ca7d661a4ba717627d469
 }
