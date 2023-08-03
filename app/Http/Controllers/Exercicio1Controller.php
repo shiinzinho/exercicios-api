e9137d43a5f546a4f0281baf8964769eba07fc1d
shiinzinho <gabrieldosantoslacerda@gmail.com>
2023-08-03 09:36:06 -0300

    ex 1 upd

--- app/Http/Controllers/Exercicio1Controller.php
+++ app/Http/Controllers/Exercicio1Controller.php
@@ -4,7 +4,6 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-<<<<<<< HEAD
 class Exercicio1Controller extends Controller
 {
     public function multiplicar(Exercício1FormRequest $request)
@@ -15,16 +14,5 @@ class Exercicio1Controller extends Controller
         return json_encode([
             'resultado' => $primeiroNumero * $segundoNumero
         ]);
-=======
-class ExercicioUmController extends Controller
-{
-    public function multiplicar(Request $request){
-        $primeiroNumero = $request->n1;
-        $segundoNumero = $request->n2;   
-
-        return json_encode([
-            'resultado' => $primeiroNumero * $segundoNumero
-        ]); 
->>>>>>> f3e9615e8de9b7af328ca7d661a4ba717627d469
     }
 }
