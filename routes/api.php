343693fe90e2d3a9b70d26bd742e50132309fb5b
shiinzinho <gabrieldosantoslacerda@gmail.com>
2023-08-03 12:36:10 -0300

    api update

--- routes/api.php
+++ routes/api.php
@@ -9,6 +9,7 @@ use App\Http\Controllers\Exercicio5Controller;
 use App\Http\Controllers\Exercicio6Controller;
 use App\Http\Controllers\Exercicio7Controller;
 use App\Http\Controllers\Exercicio8Controller;
+use App\Http\Controllers\Exercicio9Controller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -28,14 +29,16 @@ Route::post('exercicio/dois', [Exercicio2Controller::class, 'retornarMaior']);
 
 Route::post('exercicio/tres', [Exercicio3Controller::class, 'media']);
 
-Route::post('exercicio/quatro', [Exercicio4Controller::class, 'verficar']);
+Route::post('exercicio/quatro', [Exercicio4Controller::class, 'parOuImpar']);
 
-Route::post('exercicio/cinco', [Exercicio5Controller::class, 'verificacao']);
+Route::post('exercicio/cinco', [Exercicio5Controller::class, 'divisivel']);
 
 Route::post('exercicio/seis', [Exercicio6Controller::class, 'idade']);
 
 Route::get('exercicio/sete', [Exercicio7Controller::class, 'exibirNumeros']);
 
 Route::post('exercicio/oito', [Exercicio8Controller::class, 'tabuada']);
+
+Route::post('exercicio/nove', [Exercicio9Controller::class, 'divisivelPorUser']);
 
 Route::get('exercicio/doze', [Exercicio12Controller::class, 'decrescente']);
