a0245b555ee96a32b0204b5cbbe599b72a3eca42
shiinzinho <gabrieldosantoslacerda@gmail.com>
2023-08-03 09:29:07 -0300

    api

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,14 @@
 <?php
 
+use App\Http\Controllers\Exercicio12Controller;
+use App\Http\Controllers\Exercicio1Controller;
+use App\Http\Controllers\Exercicio2Controller;
+use App\Http\Controllers\Exercicio3Controller;
+use App\Http\Controllers\Exercicio4Controller;
+use App\Http\Controllers\Exercicio5Controller;
+use App\Http\Controllers\Exercicio6Controller;
+use App\Http\Controllers\Exercicio7Controller;
+use App\Http\Controllers\Exercicio8Controller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -13,7 +22,20 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "api" middleware group. Make something great!
 |
 */
+Route::post('exercicio/um', [Exercicio1Controller::class, 'multiplicar']);
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+Route::post('exercicio/dois', [Exercicio2Controller::class, 'retornarMaior']);
+
+Route::post('exercicio/tres', [Exercicio3Controller::class, 'media']);
+
+Route::post('exercicio/quatro', [Exercicio4Controller::class, 'verficar']);
+
+Route::post('exercicio/cinco', [Exercicio5Controller::class, 'verificacao']);
+
+Route::post('exercicio/seis', [Exercicio6Controller::class, 'idade']);
+
+Route::get('exercicio/sete', [Exercicio7Controller::class, 'exibirNumeros']);
+
+Route::post('exercicio/oito', [Exercicio8Controller::class, 'tabuada']);
+
+Route::get('exercicio/doze', [Exercicio12Controller::class, 'decrescente']);
