e20b241bee9e4e1a5ab278c4be3778d90a2408ec
shiinzinho <gabrieldosantoslacerda@gmail.com>
2023-08-03 12:34:25 -0300

    ex 3 FR upd

---
+++ app/Http/Requests/Exercicio3FormRequest.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Contracts\Validation\Validator;
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Http\Exceptions\HttpResponseException;
+
+class Exercicio3FormRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'n1'=> 'required | numeric',
+            'n2'=> 'required | numeric',
+            'n3'=> 'required | numeric',
+        ];
+    }
+
+    public function failedValidation(Validator $validator){
+        throw new HttpResponseException(
+            response()->json([
+                'sucess' => false,
+                'error' => $validator->errors()
+            ]));
+    }
+    public function messages()
+    {
+        return[
+            'n1.required' => 'Preencha o campo antes',
+            'n1.numeric' => 'Preencha o campo com números',
+            'n2.required' => 'Preencha o campo antes',
+            'n2.numeric' => 'Preencha o campo com números',
+            'n3.required' => 'Preencha o campo antes',
+            'n3.numeric' => 'Preencha o campo com números'
+        ];
+    }
+}
