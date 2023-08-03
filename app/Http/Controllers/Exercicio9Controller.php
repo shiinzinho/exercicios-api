5f51c07128a06e915f72c40bfda636bfac468da5
shiinzinho <gabrieldosantoslacerda@gmail.com>
2023-08-03 12:33:53 -0300

    ex 9 upd

--- app/Http/Controllers/Exercicio9Controller.php
+++ app/Http/Controllers/Exercicio9Controller.php
@@ -2,9 +2,25 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\Exercicio9FormRequest;
 use Illuminate\Http\Request;
 
 class Exercicio9Controller extends Controller
 {
-    //
+    public function divisivelPorUser(Exercicio9FormRequest $request)
+    {
+        $numero = $request->n1;
+
+        $numeros = [];
+
+        for ($i = 0; $i <= 100; $i++) {
+            if ($i % $numero == 0){
+            array_push($numero, $numeros);
+        }
+    }
+        return json_encode([
+            "resultado" => $numeros
+        ]);
+    }
 }
+// NAO TERMINADO
